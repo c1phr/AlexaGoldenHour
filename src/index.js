06b73bc28178e59106f146b22310d935d854cf2a
Ryan Batchelder <ryanbatch@gmail.com>
2017-07-16 09:52:20 -0400

    Fixing reference to event context

--- src/index.js
+++ src/index.js
@@ -53,9 +53,9 @@ exports.handler = function(event, context, callback) {
     console.log("EVENT: ", event);
     console.log("CONTEXT: ", context);
     var alexa = Alexa.handler(event, context);
-    if (context.System) {
-        deviceId = context.System.device.deviceId;
-        consentToken = context.System.user.permissions.consentToken;
+    if (event.context.System) {
+        deviceId = event.context.System.device.deviceId;
+        consentToken = event.context.System.user.permissions.consentToken;
     }    
     alexa.APP_ID = APP_ID;
     alexa.registerHandlers(handlers);
